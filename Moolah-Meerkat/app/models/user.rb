61caa05c7abb203d035555974ff6d1555b850aa6
My Name <myemail@example.com>
2013-11-11 18:16:16 -0500

    rake db:migrate to get rid of  password digest

--- Moolah-Meerkat/app/models/user.rb
+++ Moolah-Meerkat/app/models/user.rb
@@ -14,15 +14,16 @@ class User < ActiveRecord::Base
 	has_many :transactions, :through => :goals
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.email = auth.info.email
-      user.photo_url = auth.info.photo_url
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
+      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.first_name = auth.info.first_name
+        user.last_name = auth.info.last_name
+        user.email = auth.info.email
+        user.photo_url = auth.info.photo_url
+        user.oauth_token = auth.credentials.token
+        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+        user.save!
+      end
     end
   end
--- Moolah-Meerkat/db/seeds.rb
+++ Moolah-Meerkat/db/seeds.rb
@@ -1,7 +1,7 @@
 User.delete_all
 Goal.delete_all
 
-user = User.create(first_name: "Arun", last_name: "Umap", email: "arunumap@gmail.com", password_digest: "helllo", photo_url: "http://www.google.com", total_savings: 4)
+user = User.create(first_name: "Arun", last_name: "Umap", email: "arunumap@gmail.com", photo_url: "http://www.google.com", total_savings: 4)
 # User.create!(first_name: "Ernie", last_name: "Casilla", email: "ernie@gmail.com", password_digest: "haha", photo_url: "http://www.yahoo.com", total_savings: 5)
 # User.create!(first_name: "Brad", last_name: "Puder", email: "brad@gmail.com", password_digest: "what", photo_url: "http://www.cnn.com", total_savings: 6)
 # User.create!(first_name: "Paris", last_name: "Hyun", email: "paris@gmail.com", password_digest: "yes", photo_url: "http://www.buzzfeed.com", total_savings: 7)
