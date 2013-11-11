ee3224fa325d468f829506f80207f738b7446b71
My Name <myemail@example.com>
2013-11-11 14:28:29 -0500

    fixed routes

--- Moolah-Meerkat/config/routes.rb
+++ Moolah-Meerkat/config/routes.rb
@@ -1,12 +1,6 @@
 MoolahMeerkat::Application.routes.draw do
   
   root 'welcome#index'
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
