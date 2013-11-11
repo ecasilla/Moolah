94e674e41f5adaf7263627f565e2385f791f76d6
Arun Umapathy <arunumap@gmail.com>
2013-11-11 18:44:40 -0500

    seeding_not done yet but progress

--- Moolah-Meerkat/db/seeds.rb
+++ Moolah-Meerkat/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Goal.delete_all
+
+user = User.create(first_name: "Arun", last_name: "Umap", email: "arunumap@gmail.com", password_digest: "helllo", photo_url: "http://www.google.com", total_savings: 4)
+user2 = User.create(first_name: "Arun", last_name: "Umap", email: "arunumap@gmail.com", password_digest: "helllo", photo_url: "http://www.google.com", total_savings: 4)
+goal = Goal.create(name: "going to europe", description: "save 20", final_amount: 20, deadline: "10/20/1986")
+user.goals << goal
+
