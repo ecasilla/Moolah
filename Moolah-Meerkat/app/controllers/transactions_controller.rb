a30430018318cfd2b6c38cb889d3511f6a5f887e
Brad Puder <bradleypuder2015@u.northwestern.edu>
2013-11-11 14:25:23 -0500

    added controllers

---
+++ Moolah-Meerkat/app/controllers/transactions_controller.rb
@@ -0,0 +1,24 @@
+class TransactionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+end
---
+++ Moolah-Meerkat/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+end
