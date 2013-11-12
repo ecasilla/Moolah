6d78045dcdfc005452168185e3e6241fe682221e
Ernie Casilla <ecasilla@icloud.com>
2013-11-12 13:45:49 -0500

    seed fix

--- Moolah-Meerkat/db/seeds.rb
+++ Moolah-Meerkat/db/seeds.rb
@@ -1,8 +1,8 @@
 User.delete_all
 Goal.delete_all
 
-User.create(first_name: "Arun", last_name: "Umap", email: "arunumap@gmail.com", photo_url: "http://www.google.com", total_savings: 4)
-User.create(first_name: "blah", last_name: "blahblah", email: "blah@gmail.com", photo_url: "http://www.google.com", total_savings: 4)
+user =  User.create(first_name: "Arun", last_name: "Umap", email: "arunumap@gmail.com", photo_url: "http://www.google.com", total_savings: 4)
+user2 = User.create(first_name: "blah", last_name: "blahblah", email: "blah@gmail.com", photo_url: "http://www.google.com", total_savings: 4)
 
 # User.create!(first_name: "Ernie", last_name: "Casilla", email: "ernie@gmail.com", password_digest: "haha", photo_url: "http://www.yahoo.com", total_savings: 5)
 # User.create!(first_name: "Brad", last_name: "Puder", email: "brad@gmail.com", password_digest: "what", photo_url: "http://www.cnn.com", total_savings: 6)
