e93ae8a015357a09ed4d827cd8be1f91f24b5eef
My Name <myemail@example.com>
2013-11-11 19:25:27 -0500

    working code on new goal form

--- Moolah-Meerkat/app/controllers/goals_controller.rb
+++ Moolah-Meerkat/app/controllers/goals_controller.rb
@@ -6,31 +6,44 @@ class GoalsController < ApplicationController
   end
 
   def create
-    @goal = Goal.new
+    @user = User.find_by(params[:id])
+    @goal = Goal.new(goal_params)
     # add new goal to the current user
     @user.goals << @goal
+    if @goal.save
+      redirect_to user_goal_path(@user, @goal)
+    else
+      redirect_to new_user_goal_path(@user)
+    end
 
    # for adding connections to group goal
    # @connections = User.connections.all
    # add user's friends to new goal
    # @goal.users << params[:friend_id]
-    render :new
   end
 
   def show
-    @goal = User.goals.find(params[:id])
+    # @user = User.find(params[:id])
+    @goal = current_user.goals.find(params[:goal_id])
   end
 
   def index
   end
 
   def update
+    redirect_to user_path
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:name, :final_amount)
   end
 
 end
