b7110361a14bbc6297c7632bf0530a16346bbf85
My Name <myemail@example.com>
2013-11-12 21:08:01 -0500

    application controller merge conflicts resolved

--- Moolah-Meerkat/app/controllers/application_controller.rb
+++ Moolah-Meerkat/app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionsHelper
-<<<<<<< HEAD
   require 'Date'
-=======
   include PublicActivity::StoreController
->>>>>>> master
 end
