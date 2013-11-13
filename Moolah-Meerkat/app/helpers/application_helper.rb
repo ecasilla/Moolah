39bf7894712c6a1585ce4c55d9a45492fa9b60fe
My Name <myemail@example.com>
2013-11-13 10:38:36 -0500

    random background

--- Moolah-Meerkat/app/helpers/application_helper.rb
+++ Moolah-Meerkat/app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def random_background
+    ["http://www.jetblue.com/img/vacations/destination/Cancun-960-x-420.jpg", "http://www.hankstruckpictures.com/pix/trucks/david_miller/dm_cowen_truck_line_kw.jpg"].sample
+  end
+  
 end
