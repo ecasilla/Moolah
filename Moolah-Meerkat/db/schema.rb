86c15c137ccf90eb73aea3b8e36f272ca4e2fedd
My Name <myemail@example.com>
2013-11-12 21:07:01 -0500

    transaction.rb merge conflict resolved

--- Moolah-Meerkat/db/schema.rb
+++ Moolah-Meerkat/db/schema.rb
@@ -48,10 +48,12 @@ ActiveRecord::Schema.define(version: 20131112213706) do
   end
 
   create_table "goals", force: true do |t|
-    t.string  "name",         null: false
-    t.text    "description"
-    t.integer "final_amount", null: false
-    t.date    "deadline"
+    t.string   "name",         null: false
+    t.text     "description"
+    t.integer  "final_amount", null: false
+    t.date     "deadline"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "goals_users", force: true do |t|
