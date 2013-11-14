c405940eaf6077c5fb311221af34049673ba3ece
My Name <myemail@example.com>
2013-11-14 15:07:10 -0500

    working code on progress bars

--- Moolah-Meerkat/app/models/goal.rb
+++ Moolah-Meerkat/app/models/goal.rb
@@ -7,6 +7,7 @@ class Goal < ActiveRecord::Base
   include PublicActivity::Model
   tracked owner: ->(controller, model) { controller && controller.current_user }
 
+  # logic for calculating total transaction amount
   def balance(id)
     balance = 0
 
@@ -19,11 +20,22 @@ class Goal < ActiveRecord::Base
     balance
   end
 
-  def progress(user)
-    progress = (balance(user.id).to_f / self.final_amount) * 100
-    return "#{progress}%"
+  # logic for an individual's goal within a group goal.
+  def individual_final_amount
+    self.final_amount / self.users.length
   end
 
+  def progress
+    colors = ["red", "blue", "orange", "pink", "yellow", "brown", "purple", "darkgoldenrod", "green", "beige"]
+    count = 0
+    progress = {}
+    self.users.each do |user|
+      individual_progress = (balance(user.id).to_f / self.individual_final_amount) * 100
+      progress[user.id] = {:individual_progress => "#{individual_progress}%", :color => colors[count], :individuals_progress_for_goal => "#{individual_progress / self.users.length}%" }
+      count +=1
+    end
+    return progress
+  end
 
   def transform_date
     d = Date.parse(self.deadline.to_s)
